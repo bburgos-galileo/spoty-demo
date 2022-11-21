f3641d48e228edf275ebf5cdfaa5683db9e0cc49
Byron Burgos <9713763@galileo.edu>
2022-11-20 20:20:29 -0600

    se agregó boton de logout a home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { SpotifyService } from './../services/spoty.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomePage {
 
   featuredPlaylists;
 
-  constructor(private spotifyService: SpotifyService, private router: Router,) {
+  constructor(private spotifyService: SpotifyService, private router: Router, private authService: AuthService,) {
     this.getPlayList();
   }
 
@@ -23,6 +24,11 @@ export class HomePage {
   viewDetail(playlist){
 		console.log('image clicked');
 		this.router.navigate(['/detail'], {queryParams: {data: JSON.stringify(playlist)}} );
+	}
+
+  async logout() {
+		await this.authService.logout();
+		this.router.navigateByUrl('/', { replaceUrl: true });
 	}
 
 }
